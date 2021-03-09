c84ba4e7319d34dfe873855cdec720e82fa2c5ed
Patricia Alves <patricia.18_08@hotmail.com>
2021-03-09 09:34:10 -0300

    Index teste

--- src/main/java/curso/springboot/IndexController.java
+++ src/main/java/curso/springboot/IndexController.java
@@ -1,5 +1,13 @@
 package curso.springboot;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class IndexController {
+		
+		@RequestMapping("/")
+		public String index() {
+			return "index";
+		}
 }
