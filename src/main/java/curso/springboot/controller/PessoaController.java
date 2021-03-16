ba368a19f8e8fa6de73e21ab0928cda3564f33eb
Patricia Alves <patricia.18_08@hotmail.com>
2021-03-16 11:07:41 -0300

    Editando cadastro com thymeleaf

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -2,11 +2,17 @@ package curso.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
 
+@RestController
 @Controller
 public class PessoaController {
 	
@@ -14,10 +20,25 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
+	@ResponseBody
 	public String inicio() {
 		return "cadastro/cadastropessoa";
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	@ResponseBody
+	public String salvar(Pessoa pessoa) {
+		pessoaRepository.save(pessoa);
+		return"cadastro/cadastropessoa";
+	}
+	@RequestMapping(method =  RequestMethod.GET, value="/listapessoas")
+	@ResponseBody
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		return andView;
+		
+	}
 
 }
