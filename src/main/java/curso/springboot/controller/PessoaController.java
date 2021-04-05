bbc083e8d8b5ea360268953ab9c50b6db4c36ca3
Patrícia Alves <patricia.18_08@hotmail.com>
2021-04-05 06:23:41 -0300

    Model and Repository

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,8 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,17 +26,35 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
+	@RequestMapping(method = RequestMethod.GET, value = "**/cadastropessoa")
 	@ResponseBody
 	public String inicio() {
 		return "cadastro/cadastropessoa";
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
 	@ResponseBody
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			modelAndView.addObject("pessoas", pessoasIt);
+			modelAndView.addObject("pessoabj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			for (ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage());
+				
+			}
+			return modelAndView;
+		}
 		pessoaRepository.save(pessoa);
-		return"cadastro/cadastropessoa";
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoabj", new Pessoa());
+		return andView;
 	}
 	@RequestMapping(method =  RequestMethod.GET, value="/listapessoas")
 	@ResponseBody
--- src/main/java/curso/springboot/model/Pessoa.java
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -7,16 +7,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 
 @Entity
-public class Pessoa implements Serializable {
+public class Pessoa implements Serializable{
 	
+	
+
 		private static final long serialVersionUID = 1L;
 	
 		@Id
 		@GeneratedValue(strategy = GenerationType.AUTO)
 		private Long id;
 		
+		@SuppressWarnings("deprecation")
+		@NotEmpty(message = "Nome não pode ser nulo")
 		private String nome;
 		
 		private String sobrenome;
@@ -45,6 +51,41 @@ public class Pessoa implements Serializable {
 			this.sobrenome = sobrenome;
 		}
 		
-		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+			result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Pessoa other = (Pessoa) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			if (nome == null) {
+				if (other.nome != null)
+					return false;
+			} else if (!nome.equals(other.nome))
+				return false;
+			if (sobrenome == null) {
+				if (other.sobrenome != null)
+					return false;
+			} else if (!sobrenome.equals(other.sobrenome))
+				return false;
+			return true;
+		}
 		
 }
